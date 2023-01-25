45ded2dd53652b6ae371ed6bb689f74f58784f20
sunnyman <sunnyman@users.noreply.github.com>
2023-01-25 14:30:27 +0900

    新規Storyboard作成

---
+++ UikitAutoLayout/FirstView/FirstView.swift
@@ -0,0 +1,24 @@
+//
+//  FirstView.swift
+//  UikitAutoLayout
+//
+//  Created by 小松田晴 on 2023/01/25.
+//
+
+import UIKit
+
+class FirstView: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Storyboardのバックグラウンドカラーをブルーに変更する
+        view.backgroundColor = .systemBlue
+        
+        
+        
+        
+    }
+}
